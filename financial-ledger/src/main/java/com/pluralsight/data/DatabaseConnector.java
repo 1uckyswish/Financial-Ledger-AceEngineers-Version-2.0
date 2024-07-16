8ce1ae73deb979dc18f9bcaf6ce0a11033faa6bf
Noel Guillen <noelblas274@gmail.com>
2024-07-16 12:09:55 -0400

    new url format added for DB connection

--- financial-ledger/src/main/java/com/pluralsight/data/DatabaseConnector.java
+++ financial-ledger/src/main/java/com/pluralsight/data/DatabaseConnector.java
@@ -3,24 +3,25 @@ package com.pluralsight.data;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
- * Database connector
+ * Utility class for connecting to the MySQL database.
  */
 public class DatabaseConnector {
 
     /**
-     * Url of the database
+     * URL of the MySQL database.
      */
     private static final String DB_URL = "jdbc:mysql://localhost:3306/aceBank";
 
     /**
-     * Connect to the database
-     * @return BasicDataSource
+     * Connects to the MySQL database using DBCP BasicDataSource.
+     *
+     * @return BasicDataSource object configured with database connection details.
      */
     public static BasicDataSource connect() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl(DB_URL);
-        dataSource.setUsername("root");
-        dataSource.setPassword("password");
+        dataSource.setUsername("root"); // Replace with actual database username
+        dataSource.setPassword("password"); // Replace with actual database password
         return dataSource;
     }
 }
